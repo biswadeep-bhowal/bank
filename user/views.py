d92343f35cdc19b4885a8d5d705a7691c73df6b6
biswadeep-bhowal <biswadeep.bhowal@gmail.com>
2022-01-22 01:49:51 +0530

    1:55AM

--- user/views.py
+++ user/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from transaction import Transaction
-from user import Customer
+from .models import Customer
 
 
 def Login( request ) : return render( request, 'login.html', {} )
