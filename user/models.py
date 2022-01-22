7d5c8f90d99e468944546be1bfdd84f5954413d0
biswadeep-bhowal <biswadeep.bhowal@gmail.com>
2022-01-22 17:35:01 +0530

    5:30PM

--- user/models.py
+++ user/models.py
@@ -6,3 +6,5 @@ class Customer( models.Model ) :
 
     user = models.OneToOneField( User, on_delete = models.CASCADE )
     cust_id = models.BigIntegerField( primary_key = True, default = 100000 )
+
+    def str( self ) : return self.user.get_full_name()
